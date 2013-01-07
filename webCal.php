8e787d2730730bb3a1d3ee4b898964dbda0fd9a8
Ishtiaque-Shaad <shaadjoe7@gmail.com>
2013-01-07 08:41:19 -0800

    Create webCal.php

---
+++ webCal.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="SAKIL" />
+<link rel="stylesheet" href="saju.css"/>
+	<title>Untitled 3</title>
+    
+    
+    
+    
+    
+</head>
+<form action="webCal_submission.php"  method="post">
+<body>
+<table>
+<tr> <td> Calname:</td>
+<td><input type="text" name="name" /></td>
+</tr>
+<tr>
+<td>calldate:</td>
+<td><input type="text" name="date"/></td>
+</tr>
+<tr>
+<td>Call Description:</td>
+<td><textarea name="des" rows="10" cols="50"></textarea></td>
+</tr>
+<tr><td><input type="submit" name="submit" value="submit"/></td></tr>
+</table>
+</form>
+
+</body>
+</html>
